4551c1b40d189f166643c29f00d238d0e576f43a
Tanuj Thakur <tanujthakur634@gmail.com>
2026-07-15 09:56:38 +0530

    Added lap feature in JS

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const startBtn = document.getElementById("start-stop");
 const resetBtn = document.getElementById("reset");
-const lapBtn = document.getElementById("lap");
 const display = document.getElementById("display");
+const lapBtn = document.getElementById("lap");
+const list=document.getElementById("list");
 
 let startTime;
 let elapsedTime = 0;
@@ -28,9 +29,35 @@ function toggleClock() {
     clearInterval(intervalID);
     startBtn.textContent = "▶";
   } else {
-    startTime = Date.now()-elapsedTime;
+    startTime = Date.now() - elapsedTime;
+    updateClock();
     intervalID = setInterval(updateClock, 10);
     isRunning = true;
     startBtn.textContent = "⏸";
   }
 }
+
+let counter=1;
+resetBtn.addEventListener("click", () => {
+  if (isRunning) {
+    isRunning = false;
+    startBtn.textContent = "▶";
+    clearInterval(intervalID);
+  }
+  elapsedTime=0;
+  display.textContent="00:00:00:000";
+  list.innerHTML="";
+  counter=1;
+});
+
+lapBtn.addEventListener("click",showList)
+function showList(){
+  const lapTIme=display.textContent;
+  const lapList=document.createElement('li');
+  lapList.textContent="Lap"+counter+" : "+lapTIme;
+  list.appendChild(lapList);
+  counter++;
+}
+
+
+
